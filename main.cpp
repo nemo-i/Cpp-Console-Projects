516a4ebd0d5f9d1ab41c539d26aaa3e50fabdc27
nemo-i <tanton509@outlook.com>
2024-02-07 23:34:17 +0200

    Complete Delete Screen

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 #include <vector>
 #include <fstream>
+#include <iomanip>
 struct  sClient
 {
 	string name;
@@ -12,7 +13,19 @@ struct  sClient
 	bool isMarkForDelete = false;
 	bool isMarkForUpdate = false;
 };
-
+enum enOptions {
+	Show = 1,
+	Add = 2,
+	Delete = 3,
+	Update = 4,
+	Find = 5,
+	Exit = 6,
+};
+void ShowClientList(vector<sClient> clients);
+void DrawAddNewClientScreen(std::vector<sClient>& clients);
+void DrawClientInfoCard(sClient& client);
+void DrawClientDeleteScreen(std::vector<sClient>& clients);
+void DrawMainMenu();
 string ConvertClientToRecord(sClient client,string sep = "#//#") {
 	string record;
 	record += client.name + sep;
@@ -200,13 +213,13 @@ void ReadClientFromUserWithAccountNumber(vector<sClient> &clients,sClient &clien
 	getline(cin>>ws,accountNumber);
 	if (FindClientByAccountNumber(clients, accountNumber, client)) {
 		cout << "Client With Account Number [" << client.accountNumber << "] Already Exits\n";
-		return;
+		
 	}
 	else
 	{
-		client = ReadClientFromUserWithoutAccountNumber(client);
+		ReadClientFromUserWithoutAccountNumber(client);
 		client.accountNumber = accountNumber;
-		return;
+		
 	}
 }
 
@@ -224,6 +237,7 @@ void ReadAndAddClientToClientListAndDatabase() {
 		cout << "Are you want to add more clients Y/N? ";
 		cin >> addMore;
 	} while (addMore =='Y' || addMore == 'y');
+	
 }
 
 
@@ -267,6 +281,7 @@ void DeleteClientAndSaveToDatabase(vector<sClient> clients) {
 	char sure;
 	bool found = FindClientByAccountNumber(clients, accountNumber, client);
 	if (found) {
+		DrawClientInfoCard(client);
 		cout << "Are you sure want to delete this client Y/N? ";
 		cin >> sure;
 		if (sure == 'Y' || sure == 'y') {
@@ -275,11 +290,13 @@ void DeleteClientAndSaveToDatabase(vector<sClient> clients) {
 			cout << "Client Deleted Successfully!\n";
 			cout << "Press any key to go to main menu... ";
 			system("pause>nul");
+			DrawMainMenu();
 		}
 		else
 		{
 			cout << "Press any key to go to main menu... ";
 			system("pause>nul");
+			DrawMainMenu();
 		}
 	}
 	else
@@ -287,9 +304,163 @@ void DeleteClientAndSaveToDatabase(vector<sClient> clients) {
 		cout << "Client not found\n";
 		cout << "Press any key to go to main menu... ";
 		system("pause>nul");
+		DrawMainMenu();
+	}
+}
+std::string AddSpace(short count) {
+	std::string space;
+	for (size_t i = 0; i < count; i++)
+	{
+		space += " ";
+	}
+	return space;
+}
+void DrawMainMenuHeader() {
+	cout << "=================================" << endl;
+	cout << AddSpace(9) << "Main Menu Screen" << AddSpace(9) << endl;
+	cout << "=================================" << endl;
+}
+void DrawMainMenuOption(short index, string title)
+{
+	cout << AddSpace(5) << "[" << index + 1 << "] " << title << "." << endl;
+}
+void DrawOptions() {
+	string options[6] = {
+	   "Show Client List",
+	   "Add New Client",
+	   "Delete Client",
+	   "Update Client",
+	   "Find Client",
+	   "Exit",
+	};
+
+	for (size_t i = 0; i < 6; i++)
+	{
+		DrawMainMenuOption(i, options[i]);
 	}
 }
 
+void ShowMenus(enOptions option)
+{
+	vector<sClient> clients = ReadClientsFromDatabase();
+	switch (option)
+	{
+	case Show:
+		ShowClientList(clients);
+		break;
+	case Add:
+		DrawAddNewClientScreen(clients);
+		break;
+	case Delete:
+		DrawClientDeleteScreen(clients);
+		break;
+	case Update:
+		break;
+	case Find:
+		break;
+	case Exit:
+		break;
+	default:
+		break;
+	}
+}
+void Clear() {
+	system("cls");
+}
+short ReadOption() {
+	std::cout << "Choose what do you want to do? [1 to 6]?";
+	short option;
+	std::cin >> option;
+	return option;
+}
+void DrawMainMenu() {
+	Clear();
+	DrawMainMenuHeader();
+	DrawOptions();
+	cout << "=================================" << endl;
+	enOptions option = (enOptions)ReadOption();
+	ShowMenus(option);
+}
+std::string GenerateLine(short count)
+{
+	std::string line;
+	for (size_t i = 0; i < count; i++)
+	{
+		line += "-";
+	}
+	return line;
+}
+
+void DrawLine(short length) {
+	std::cout << GenerateLine(length) << std::endl;
+};
+
+void DrawClientRow(sClient& client) {
+	std::cout << "|" << std::setw(15) << client.accountNumber << "|" << std::setw(15) << client.pincode << "|" << std::setw(28) << client.name  << "|" << std::setw(15) << client.balance << "|" << std::endl;
+}
+void DrawClientListHeader(short clients)
+{
+
+	DrawLine(100);
+	std::cout << AddSpace(30) << "Client List (" << clients << ") Client(s)." << AddSpace(10) << std::endl;
+	DrawLine(100);
+	std::cout << "|" <<setw(15) << "Account Number" << "|" << std::setw(15) << "Pin Code" << "|" << std::setw(28) << "Client Name"  << "|" << std::setw(15) << "Balance" << "|" << std::endl;
+	DrawLine(100);
+
+}
+
+void ShowClientList(vector<sClient> clients) {
+
+	Clear();
+	DrawClientListHeader(clients.size());
+	for (auto& i : clients)
+	{
+		DrawClientRow(i);
+	}
+	DrawLine(100);
+	std::cout << "Press any key to go to main menu..." << std::endl;
+	system("pause>nul");
+	DrawMainMenu();
+}
+
+void DrawAddNewClientHeader() {
+	DrawLine(50);
+	std::cout << AddSpace(15) << "Add New Client Screen" << AddSpace(15) << std::endl;
+	DrawLine(50);
+}
+void DrawAddNewClientScreen(std::vector<sClient>& clients) {
+	sClient client;
+	std::string record;
+	std::string addMore;
+	Clear();
+	DrawAddNewClientHeader();
+	ReadAndAddClientToClientListAndDatabase();
+	cout << "Press any key to go to main menu? ";
+	system("pause>nul");
+	DrawMainMenu();
+}
+void DrawDeleteClientHeader() {
+	DrawLine(50);
+	std::cout << AddSpace(15) << "Delete Client Screen" << AddSpace(15) << std::endl;
+	DrawLine(50);
+}
+void DrawClientInfoCard(sClient& client) {
+	std::cout << "The following are the client details:" << std::endl;
+	DrawLine(35);
+	std::cout << "Account Number:" << client.accountNumber << std::endl;
+	std::cout << "Pin Code      :" << client.pincode << std::endl;
+	std::cout << "Name          :" << client.name << std::endl;
+	std::cout << "Balance       :" << client.balance << std::endl;
+	DrawLine(35);
+};
+void DrawClientDeleteScreen(std::vector<sClient>& clients) {
+	Clear();
+	std::string accountNumber;
+	sClient client;
+	char deleteClient;
+	DrawDeleteClientHeader();
+	DeleteClientAndSaveToDatabase(clients);
+}
 
 
 int main() {
@@ -328,6 +499,7 @@ clients =ReadClientsFromDatabase();
 //}
 	//ReadAndAddClientToClientListAndDatabase();
 	//UpdateClientAndSaveToDatabase(clients);
-DeleteClientAndSaveToDatabase(clients);
+//DeleteClientAndSaveToDatabase(clients);
+DrawMainMenu();
 	return 0;
 }
